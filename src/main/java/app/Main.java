f11ae9199b8b0d458e75bbd9141abdc9784cecde
HeckNaw <natealexmills@icloud.com>
2025-11-17 13:55:40 -0500

    imported fonts, added jersey10 font, created fontloader to load all fonts in main so it only has to be done once. changed signupview and loginview so all buttons and text use font

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,9 +1,12 @@
 package app;
 
+import view.FontLoader;
+
 import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
+        FontLoader.loadFonts(); // load all fonts in, should be done first
         AppBuilder appBuilder = new AppBuilder();
         JFrame application = appBuilder
                 .addLoginView()
--- src/main/java/view/SignupView.java
+++ src/main/java/view/SignupView.java
@@ -35,22 +35,34 @@ public class SignupView extends JPanel implements ActionListener, PropertyChange
         signupViewModel.addPropertyChangeListener(this);
 
         final JLabel title = new JLabel(SignupViewModel.TITLE_LABEL);
+        title.setFont(FontLoader.jersey10.deriveFont(Font.BOLD, 24f));
         title.setAlignmentX(Component.CENTER_ALIGNMENT);
 
+        final JLabel usernameLabel = new JLabel("Username:");
+        usernameLabel.setFont(FontLoader.jersey10.deriveFont(Font.BOLD, 18f));
+        final JLabel passwordLabel = new JLabel("Password:");
+        passwordLabel.setFont(FontLoader.jersey10.deriveFont(Font.BOLD, 18f));
+        final JLabel repeatPasswordLabel = new JLabel("Repeat Password:");
+        repeatPasswordLabel.setFont(FontLoader.jersey10.deriveFont(Font.BOLD, 18f));
+
         final LabelTextPanel usernameInfo = new LabelTextPanel(
-                new JLabel(SignupViewModel.USERNAME_LABEL), usernameInputField);
+                usernameLabel, usernameInputField);
         final LabelTextPanel passwordInfo = new LabelTextPanel(
-                new JLabel(SignupViewModel.PASSWORD_LABEL), passwordInputField);
+                passwordLabel, passwordInputField);
         final LabelTextPanel repeatPasswordInfo = new LabelTextPanel(
-                new JLabel(SignupViewModel.REPEAT_PASSWORD_LABEL), repeatPasswordInputField);
+                repeatPasswordLabel, repeatPasswordInputField);
 
         final JPanel buttons = new JPanel();
         toLogin = new JButton(SignupViewModel.TO_LOGIN_BUTTON_LABEL);
+        toLogin.setFont(FontLoader.jersey10.deriveFont(Font.BOLD, 18f));
         buttons.add(toLogin);
         signUp = new JButton(SignupViewModel.SIGNUP_BUTTON_LABEL);
+        signUp.setFont(FontLoader.jersey10.deriveFont(Font.BOLD, 18f));
         buttons.add(signUp);
         cancel = new JButton(SignupViewModel.CANCEL_BUTTON_LABEL);
+        cancel.setFont(FontLoader.jersey10.deriveFont(Font.BOLD, 18f));
         buttons.add(cancel);
+
 
         ImagePanel background = new ImagePanel("/images/LoginScreenBackground.png");
         background.setLayout(new BoxLayout(background, BoxLayout.Y_AXIS));
