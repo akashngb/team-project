bae4a6671d52adc6d48222c5905ffee67c493218
KaiYT1 <kaibague@gmail.com>
2025-11-30 02:21:19 -0500

    Finally updated DAOs to use LeaderBoard for users

--- src/main/java/entity/LeaderBoard.java
+++ src/main/java/entity/LeaderBoard.java
@@ -13,51 +13,16 @@ public class LeaderBoard {
     private final ArrayList<User> leaderboard;
 
     /**
-     * Creates a new LeaderBoard with the given params. A specific User is associated
-     * to its highscores
+     * Creates a new LeaderBoard with the given params.
      * @param Users the list of users
-     * @param Highscores their highscores
      * @throws IllegalArgumentException if any params are empty
      */
-    public LeaderBoard(ArrayList<Map<String, Integer>> Highscores, ArrayList<User> Users){
-        leaderboard = new HashMap<>();
-        for (int i = 0; i < Users.size(); i++) {
-            User user = Users.get(i);
-            Map<String, Integer> highscores = Highscores.get(i);
-            leaderboard.put(user, highscores);
-        };
+    public LeaderBoard(ArrayList<User> Users){
+        if (Users == null) {
+            throw new IllegalArgumentException("Parameters cannot be null");
+        }
+        this.leaderboard = Users;
     }
 
-    public Map<User, Map<String, Integer>> getLeaderboard() { return leaderboard; }
-
-    public ArrayList<User> getUsersWithLessScoreInGame(Integer score, String game) {
-        try {
-            Games.valueOf(game);
-            ArrayList<User> users = new ArrayList<>();
-            for (Map.Entry<User, Map<String, Integer>> entry : leaderboard.entrySet())  {
-                if (entry.getValue().get(game) < score) {
-                    users.add(entry.getKey());
-                }
-            }
-            return users;
-        }
-        catch (IllegalArgumentException e) {
-            throw new IllegalArgumentException("No such game");
-        }
-    }
-
-    public Integer getBestScore() {
-        Integer bestScore = 0;
-        for (Map.Entry<Integer, User> entry : leaderboard.entrySet()) {
-            if (entry.getKey() > bestScore) {bestScore = entry.getKey();}
-        }
-        return bestScore;
-    }
-
-    public Integer getHighScore(User user) {
-        for (Map.Entry<Integer, User> entry : leaderboard.entrySet()) {
-            if (entry.getValue().equals(user)) { return entry.getKey();}
-        }
-        throw new IllegalArgumentException("No such user");
-    }
+    public ArrayList<User> getLeaderboard() { return leaderboard; }
 }
--- src/main/java/use_case/leaderboard/LeaderBoardDataAccessInterface.java
+++ src/main/java/use_case/leaderboard/LeaderBoardDataAccessInterface.java
@@ -2,10 +2,14 @@ package use_case.leaderboard;
 
 import entity.User;
 
+import java.util.List;
+
 public interface LeaderBoardDataAccessInterface {
     /**
      *  The DAO for LeaderBoard actions
      * @param user whom we will change the highscore from
      */
     void changeHighscore(User user);
+
+    List<User> getTopUsersForGame(String gameName, int limit);
 }
